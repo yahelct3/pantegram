7dc6271d2cccf6c74aa47615920ef24a1d1da03e
LotionDude <lotan07mc@gmail.com>
2023-04-10 00:35:43 +0300

    feat(Sidebar.tsx): add component

---
+++ client/src/components/sidebar.tsx
@@ -0,0 +1,28 @@
+import { Navbar } from "react-daisyui";
+
+export default function SidebarComponent() {
+  return (
+    <div className="drawer drawer-mobile">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden md:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 bg-base-300 text-base-content">
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import NavbarComponent from "../components/navbar";
+import SidebarComponent from "../components/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <NavbarComponent></NavbarComponent>;
+  return (
+    <div>
+      <NavbarComponent></NavbarComponent>
+    </div>
+  );
 }
