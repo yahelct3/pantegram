1ba483b75564cb37334fe9d6cdac7de3aa59f968
LotionDude <lotan07mc@gmail.com>
2023-04-10 00:03:40 +0300

    init project

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Button } from "react-daisyui";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <div></div>;
+}
